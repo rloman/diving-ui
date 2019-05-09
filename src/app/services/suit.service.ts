93a9210da5d8b729fc3dca2ed8de4cb846fb84cc
Raymond Loman <raymond@carpago.nl>
2019-05-09 14:48:21 +0200

    Fix a bug (invalid import)

--- src/app/services/suit.service.ts
+++ src/app/services/suit.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Suit } from '../model/suit';
 import { Observable } from 'rxjs';
-import { create } from 'domain';
 
 @Injectable({
   providedIn: 'root'
