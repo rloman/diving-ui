773e419dd12c98ce3f14b7d8f8e841344ab57724
Raymond Loman <raymond@carpago.nl>
2019-05-09 14:18:02 +0200

    Add content of SuitService wit the GET::findAll method

--- src/app/services/suit.service.ts
+++ src/app/services/suit.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Suit } from '../model/suit';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SuitService {
 
-  constructor() { }
+  private url: string = "http://localhost:8080/api";
+  private endpoint: string = "suits"
+
+  // this is called the Angular way of dependency injection
+  // comparable to Spring Boot his way of using constructor based injection
+  constructor(private httpClient: HttpClient) { }
+
+  list(): Observable<Suit[]> {
+    return this.httpClient.get<Suit[]>(`${this.url}/${this.endpoint}`); // returns an Observable of type User
+  }
 }
